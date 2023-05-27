d65e4c140e931e0e2da0ae0ff65d7cfa58e217f6
João Victor da Silva <jv782063@gmail.com>
2023-05-27 10:55:44 -0300

    pega os arquivos modificados, etc, do git para poder fazer um status

--- pkg/commands/status.go
+++ pkg/commands/status.go
@@ -7,8 +7,14 @@ import (
 	"github.com/go-git/go-git/v5"
 )
 
+type Entry struct {
+  StatusEntryWorktree git.StatusCode
+  File                string
+}
+
 func Status() {
   repo, err := git.PlainOpen(".")
+
   if err != nil {
     fmt.Println("Error opening repository: ", err)
     os.Exit(1)
@@ -24,7 +30,46 @@ func Status() {
   if err != nil {
     fmt.Println("Error getting status: ", err)
     os.Exit(1)
+  } 
+
+  entries := handleWorktreeStatus(status)
+
+  for _, entry := range entries {
+    fmt.Println(entry.StatusEntryWorktree, entry.File)
+  }
+}
+
+func handleWorktreeStatus(status git.Status) []Entry {
+  entries := make([]Entry, 0)
+
+  for file, statusEntry := range status {
+    entry := Entry{
+      StatusEntryWorktree: statusEntry.Worktree,
+      File:                file,
+    }
+
+    entries = append(entries, entry)
   }
 
-  fmt.Println(status)
+  return entries
 }
+
+// func handleWorktreeStatus(worktreeStatus git.StatusCode) ([]string, []string) {
+//   untracked := make([]string, 0)
+//   modified := make([]string, 0)
+
+//   switch worktreeStatus {
+//   case git.Added:
+//     
+
+//   case git.Modified:
+//     fmt.Println("Arquivo modificado")
+
+//   case git.Unmodified:
+//     fmt.Println("Arquivo não modificado")
+
+//   default:
+//     fmt.Println("Status desconhecido")
+//   }
+// }
+
